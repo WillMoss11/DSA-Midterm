f231787f785723da7168c8beac4ef29aebc3ce21
WillMoss11 <william.moss@keyin.com>
2025-02-24 00:34:25 -0330

    user (name) added

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,10 @@
 public class User {
+    private String name;
+    private TaskList tasklist;
+
+    // Creating user with a unique name and an empty task list
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList(); // Every user has their own to-do list
+    }
 }
