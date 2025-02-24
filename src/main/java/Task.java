fbe00bc2662016c7c66bc6a6fa0f03299e775f63
WillMoss11 <william.moss@keyin.com>
2025-02-24 00:18:45 -0330

    Task Started

---
+++ src/main/java/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    // Creating a task with a description. Not completed yet
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false; // Still work in progress
+    }
+}
