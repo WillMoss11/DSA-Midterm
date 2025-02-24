0e9f5ae9a17fb5019549f5f76a2069dc214d0756
WillMoss11 <william.moss@keyin.com>
2025-02-24 00:48:41 -0330

    Task adder

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -5,4 +5,18 @@ public class TaskList {
     public TaskList() {
         head = null; // Empty list to start with
     }
+
+    // Add new task to the list. Like adding new item to the "to-do" pile
+    public void addTask(Task task) {
+        Node newNode = new Node(task); // Wreap task in a node
+        if (head == null) {
+            head = newNode; // First task, so it's the new head of the list
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next; // Go to the end of the list
+            }
+            current.next = newNode; // Add task at the end
+        }
+    }
 }
