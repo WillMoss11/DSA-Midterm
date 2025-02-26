ef7cbabd9f04e826e5a5885db7a7d5cee061a82f
WillMoss11 <william.moss@keyin.com>
2025-02-26 20:17:48 -0330

    Added Scanner start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(string[] args) {
+        Scanner scanner = new Scanner(System.in);
+        User[] users = new User[10]; // Just put a max of 10 users for example
+        int userCount = 0; // Just to keep track of the users that are created
 
+
+    }
 }
