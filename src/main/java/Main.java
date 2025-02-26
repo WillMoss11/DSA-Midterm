021989128d59bed416a0f16c87168c17849abac8
WillMoss11 <william.moss@keyin.com>
2025-02-26 20:22:54 -0330

    Created Main Menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,22 @@ public class Main {
         User[] users = new User[10]; // Just put a max of 10 users for example
         int userCount = 0; // Just to keep track of the users that are created
 
+        boolean running = true;
+        while (running) {
+            // Main Menu start
+            System.out.print("\n--- Main Menu ---");
+            System.out.print("1. Create a user");
+            System.out.print("2. View all users");
+            System.out.print("3. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Makes new line to make pretty
+
+
+                    }
+            }
+        }
+
 
     }
 }
